5abaa5ca5207c4a6d0699eff70e449da2c964f12
Steve <ycsteven.tsao@gmail.com>
2023-01-27 14:32:51 +0800

    function params default value

--- 0127/demo02.php
+++ 0127/demo02.php
@@ -6,8 +6,20 @@ echo '<br>';
 $username = 'momo';
 echo $username . '<hr>';
 //----------
+echo '<h2>function</h2>';
+echo '<h3>getUsername</h3>';
 function getUsername(string $username): string
 {
     return 'Hello ' . $username;
 }
-echo getUsername('GOGO').'<br>';
+
+echo getUsername('GOGO') . '<br>';
+echo '<hr>';
+
+//參數不足
+function getTotal(float $price, int $num = 1): float
+{
+    return $price * $num;
+}
+
+echo '總金額：' . getTotal(45) . ' 元<br>';
