c9337783756ca04278efb9f250f8af67fe832caf
Steve <ycsteven.tsao@gmail.com>
2023-01-27 22:35:50 +0800

    vsprintf,literal template

--- 0127/demo03.php
+++ 0127/demo03.php
@@ -15,3 +15,33 @@ $arr = ['red', 'green', 'blue'];
 var_dump($arr);
 echo '<br>';
 print_r($arr);
+echo '<br>';
+echo '<pre>' . print_r($arr, true) . '</pre>';
+echo '<br>';
+
+//printf(模版，資料)
+printf('<pre> %s </pre>', print_r($arr, true));
+echo '<br>';
+
+$item = ['電腦', 8000, 1];
+echo '<span style="color: chocolate">printf</span>' . '<br>';
+printf('品名：%s，單價：%d，數量：%d<br>', $item[0], $item[1], $item[2]);
+echo '<span style="color: chocolate">vprintf</span>' . '<br>';
+vprintf('品名：%s，單價：%d，數量：%d<br>', $item);
+echo '<span style="color: chocolate">vsprintf</span>' . '<br>';
+echo vsprintf('品名：%s，單價：%d，數量：%d<br>', $item);
+
+$params = ['mysql', 'localhost', 'phpedu', '3306', 'utf8'];
+/**
+ * template
+ * 'mysql:host=localhost;dbname=phpedu;port=3306;charset=utf8'
+ */
+$dsn = vsprintf('%s:host=%s;dbname=%s;port=%s;charset=%s', $params);
+echo $dsn . '<br>';
+
+$db = new \PDO($dsn, 'phpedu', 'phpedu');
+if ($db) {
+    echo 'connect success!';
+} else {
+    echo 'connect failed';
+}
