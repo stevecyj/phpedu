2e6bb582cafa4bcf261ce76c28319ca99e8ca1df
Steve <ycsteven.tsao@gmail.com>
2023-01-30 07:33:48 +0800

    double quote

--- 0129/demo03.php
+++ 0129/demo03.php
@@ -4,3 +4,23 @@ namespace _0129;
 $domain = 'https://google.com';
 $str = ' 網站名稱： \n\r $domain';
 echo $str . '<br>';
+
+$str = <<< 'TEXT'
+<header>
+<nav>
+<a href="">頁頁</a>
+</nav>
+</header>
+TEXT;
+echo $str . '<br>';
+
+$site = " 網站名稱： \n\r $domain";
+echo $site . '<hr>';
+
+$tpl = <<<"MYTPL"
+<ul style="display: inline-grid;border: 1px solid;">
+<li style="color: brown;">蕃茄</li>
+<li>$domain</li>
+</ul>
+MYTPL;
+echo $tpl;
