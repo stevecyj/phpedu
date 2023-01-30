649bf37f32c6d6d4165e7e521194a4c2f7429bda
Steve <ycsteven.tsao@gmail.com>
2023-01-30 22:58:30 +0800

    implode, explode

--- 0129/demo04.php
+++ 0129/demo04.php
@@ -1,3 +1,15 @@
 <?php
 
 namespace _0129;
+
+$colors = ['red', 'green', 'blue'];
+echo implode(', ', $colors) . '<hr>';
+
+$link = 'mysql:dbname=phpedu;root;root';
+list($dsn, $username, $pw) = explode(';', $link);
+printf('dsn=%s<br> username=%s<br> pw=%s<br>', $dsn, $username, $pw);
+echo '<hr>';
+$format = 'dsn=%s<br> username=%s<br> pw=%s<br>';
+$db = explode(';', $link);
+vprintf($format, $db);
+echo '<hr>';
